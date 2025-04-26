2a7a1e65905756710f34a30362ed755126e2dd57
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-04-26 09:46:55 +0600

    Operation & Expression

---
+++ operator-and-expression.php
@@ -0,0 +1,41 @@
+<?php 
+
+// Arithmatic Operator
+$one    = 1;
+$two    = "2";
+$three  = 3;
+$five   = "5";
+$ten    = "10";
+$seven  = 7;
+$twelve = 12;
+
+// echo $one + $five;
+// echo "<br>";
+// echo $five - $three;
+// echo "<br>";
+// echo $five * $two;
+// echo "<br>";
+// echo $ten / $two;
+// echo "<br>";
+// echo $twelve % $five;
+
+// Assignment Operator
+// $five += $three; // $five = $five + $three
+// echo $five;
+
+// Comparison Operator
+// $a = "shakib";
+// $b = "shakib";
+// var_dump( $a === $b );
+
+$num_one = 5;
+$num_two = "5";
+
+// var_dump( $num_one == $num_two ); // true
+// var_dump( $num_one === $num_two ); // false
+// var_dump( $num_one != $num_two ); // false
+// var_dump( $num_one !== $num_two ); // true
+// var_dump( $num_one < 10 && $num_one > 2 );  // true
+// var_dump( $num_one < 10 && $num_one > 6 ); // false
+// var_dump( $num_one < 10 || $num_one > 6 ); // true
+// var_dump( $num_one < 4 || $num_one > 6 || $num_one === $num_two );
