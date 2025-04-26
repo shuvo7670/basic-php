ff73aa65b047d41e3ed621a589f599ac03e651da
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-04-26 09:50:20 +0600

    Simple change

--- operator-and-expression.php
+++ operator-and-expression.php
@@ -38,4 +38,4 @@ $num_two = "5";
 // var_dump( $num_one < 10 && $num_one > 2 );  // true
 // var_dump( $num_one < 10 && $num_one > 6 ); // false
 // var_dump( $num_one < 10 || $num_one > 6 ); // true
-var_dump( $num_one < 4 || $num_one > 6 || $num_one === $num_two );
+// var_dump( $num_one < 4 || $num_one > 6 || $num_one === $num_two );
