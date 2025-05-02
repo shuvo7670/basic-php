61882db58fac8cc1da68351f929aa3b9df07ef01
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-05-02 10:15:59 +0600

    Common Functions

---
+++ common-function.php
@@ -0,0 +1,15 @@
+<?php
+// String
+
+// echo strlen('Hello World!'); // output 12
+echo "<br>";
+// echo strtoupper('upper me'); 
+echo "<br>";
+// echo strtolower('LOWER ME');
+// echo substr("Hello, World!", 7, 12); 
+// echo str_replace("world", "world PHP", "Hello world"); // Output: Hello PHP
+// echo rand(300, 500); // generate random value
+
+define("SITE_NAME", "MyWebsite");
+echo SITE_NAME;
+
