80f8e5217c55d994aa571f79453571416316049a
shuvo7670 <mdshakibul.shuvo@gmail.com>
2025-05-02 09:33:24 +0600

    Function

--- function.php
+++ function.php
@@ -24,3 +24,37 @@ function add($num_one, $num_two) {
     return $num_one + $num_two;
 }
 $add = add(5, 10);
+
+// Local scope
+function localVariable() {
+    $var = 10;
+    echo $var;
+}
+// localVariable();
+
+// Global scope
+$globalVar = "I am global variable";
+$globalVar2 = "I am from global var 2";
+function globalVar() {
+    // global $globalVar;
+    $globalVar = $GLOBALS['globalVar'];
+    $globalVar2 = $GLOBALS['globalVar2'];
+    echo $globalVar;
+    echo $globalVar2;
+}
+// globalVar();
+
+// Static scope
+function counter() {
+    static $count_me = 1;
+    $count_me++;
+    echo $count_me . "<br>";
+}
+// counter(); // result 2
+// counter(); // result 3
+
+// Anonymous function
+$anno = function($n) {
+    return $n * $n;
+};
+echo $anno(2);
